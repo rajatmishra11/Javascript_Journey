d99ce969acebfa56dff525eab0b7993fe9457fcb
Rajat Mishra <rajatmishra482001@gmail.com>
2024-03-19 20:14:17 +0530

    Update README.md to add a section on the HTML DOM and finding and modifying HTML elements. Update 6_Numbers.js to use `const` instead of `let` for variable declarations and make other minor formatting changes.

--- basics/6_Numbers.js
+++ basics/6_Numbers.js
@@ -7,21 +7,21 @@
     6.  
 */
 
-const score=400
-console.log(score)              //400
-console.log(typeof score)       //Number
+const score = 400;
+console.log(score); //400
+console.log(typeof score); //Number
 //-----------------------------------------
-const balance=new Number(100)
-console.log(balance)            //[Number : 100]
-console.log(typeof balance)     //object
+const balance = new Number(100);
+console.log(balance); //[Number : 100]
+console.log(typeof balance); //object
 //-----------------------------------------
-console.log(balance.toString().length)
+console.log(balance.toString().length);
 //-----------------------------------------
-console.log(balance.toFixed(2))  //100.00
+console.log(balance.toFixed(2)); //100.00
 //----------------------------------------
-const othernum=23.4594
-console.log(othernum.toPrecision(4))    //23.46(round-off)
+const othernum = 23.4594;
+console.log(othernum.toPrecision(4)); //23.46(round-off)
 //---------------------------------------
-const hundreds=100000
-console.log(hundreds.toLocaleString('en-IN'))       //1,00,000
+const hundreds = 100000;
+console.log(hundreds.toLocaleString("en-IN")); //1,00,000
 //---------------------------------------
